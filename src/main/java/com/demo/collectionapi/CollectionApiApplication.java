ef2d726f264e533a595206fe735667ebd2ec651b
Jakub Matras <jkob.matras@gmail.com>
2023-07-18 21:43:56 +0200

    First initial commit

---
+++ src/main/java/com/demo/collectionapi/CollectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.collectionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/collectionapi/CollectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.collectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
